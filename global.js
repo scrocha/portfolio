c3f32edaa81e6a852f21ec8fd86689b9be9069a6
Sillas Rocha <sillasrocha29@gmail.com>
2025-03-13 12:32:40 -0300

    corrected references

--- global.js
+++ global.js
@@ -13,11 +13,13 @@ if (currentLink) {
 }
 
 let pages = [
-    {url: "../", title: "Sobre Mim"},
-    {url: "../projects/", title: "Projetos"},
-    {url: "../resume/", title: "Currículo"},
-    {url: "../contact/", title: "Contato"}
+    {url: "", title: "Sobre Mim"},
+    {url: "projects/", title: "Projetos"},
+    {url: "resume/", title: "Currículo"},
+    {url: "contact/", title: "Contato"}
 ];
+
+const ARE_WE_HOME = document.documentElement.classList.contains("home");
 
 let h1 = document.createElement("h1");
 h1.textContent = document.title;
@@ -29,6 +31,10 @@ document.body.insertBefore(nav, h1.nextSibling);
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
+    
+    if (!ARE_WE_HOME && !url.startsWith("http")) {
+        url = "../" + url;
+    }
     
     nav.insertAdjacentHTML("beforeend", `<a href="${ url }">${ title }</a>`);
     if (location.pathname.includes(url)) {

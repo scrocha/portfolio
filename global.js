2ccd4000386c2f51cb00181e7ecc42498825eea1
Sillas Rocha <sillasrocha29@gmail.com>
2025-03-13 12:28:53 -0300

    added new nav bar to pages

--- global.js
+++ global.js
@@ -3,3 +3,35 @@ console.log("IT’S ALIVE!");
 function $$ (selector, context = document) {
     return Array.from(context.querySelectorAll(selector));
 }
+
+let navLinks = $$("nav a");
+
+let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname);
+
+if (currentLink) {
+    currentLink.classList.add("current");
+}
+
+let pages = [
+    {url: "../", title: "Sobre Mim"},
+    {url: "../projects/", title: "Projetos"},
+    {url: "../resume/", title: "Currículo"},
+    {url: "../contact/", title: "Contato"}
+];
+
+let h1 = document.createElement("h1");
+h1.textContent = document.title;
+document.body.prepend(h1);
+
+let nav = document.createElement("nav");
+document.body.insertBefore(nav, h1.nextSibling);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    
+    nav.insertAdjacentHTML("beforeend", `<a href="${ url }">${ title }</a>`);
+    if (location.pathname.includes(url)) {
+        document.title = title;
+    }
+}
